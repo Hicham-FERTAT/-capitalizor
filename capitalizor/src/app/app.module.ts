184018e33bb34079535c5c210f64b8f9cc4e788b
hicham.fertat <hicham.fertat@outlook.com>
2019-12-21 17:23:42 +0000

    [Capitalizor] Ajout des component home, category et profil

--- capitalizor/src/app/app.module.ts
+++ capitalizor/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LoginComponent } from './modules/login/login.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/home.component';
+import { CategoryComponent } from './modules/category/category.component';
+import { ProfilComponent } from './modules/profil/profil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    CategoryComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule
